a3f30094425deaf230fd86a350942ec2ced0b95b
Andrew Lee <andyrewlee@gmail.com>
2018-07-17 01:42:01 -0700

    Change font and add accent letter

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -2,8 +2,15 @@ import React, { Component } from 'react';
 
 export default class MainHeader extends Component {
   render() {
+    const title = this.props.title;
+    const substringIndex = title.length - 1;
+    const mainStr = title.substring(0, substringIndex);
+    const accentStr = title.substring(substringIndex);
+
     return (
-      <h1 className="MainHeader">{this.props.title}</h1>
+      <h1 className="MainHeader">
+        {mainStr}<span class="Accent">{accentStr}</span>
+      </h1>
     );
   }
 }
